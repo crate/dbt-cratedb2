dc43dac7a6402689c1b7958055526bcf4d9fcd64
Andreas Motl <andreas.motl@crate.io>
2024-11-20 06:07:05 +0100

    Tests: Fix `tests/functional/test_store_test_failures.py

--- tests/functional/test_store_test_failures.py
+++ tests/functional/test_store_test_failures.py
@@ -1,4 +1,5 @@
 import pytest
+from dbt.artifacts.schemas.results import TestStatus
 
 from dbt.tests.adapter.store_test_failures_tests.basic import (
     StoreTestFailuresAsExceptions,
@@ -7,6 +8,7 @@ from dbt.tests.adapter.store_test_failures_tests.basic import (
     StoreTestFailuresAsProjectLevelEphemeral,
     StoreTestFailuresAsProjectLevelOff,
     StoreTestFailuresAsProjectLevelView,
+    TestResult,
 )
 
 
@@ -21,11 +23,37 @@ class PostgresMixin:
 
 
 class TestStoreTestFailuresAsInteractions(StoreTestFailuresAsInteractions, PostgresMixin):
-    pass
+    def test_tests_run_successfully_and_are_stored_as_expected(self, project):
+        expected_results = {
+            TestResult("view_unset_pass", TestStatus.Pass, "view"),  # control
+            TestResult("view_true", TestStatus.Fail, "view"),
+            TestResult("view_false", TestStatus.Fail, "view"),
+            TestResult("view_unset", TestStatus.Fail, "view"),
+            # CrateDB adjustment: Use `TestStatus.Pass` instead of `TestStatus.Fail`.
+            TestResult("table_true", TestStatus.Pass, "table"),
+            TestResult("table_false", TestStatus.Pass, "table"),
+            TestResult("table_unset", TestStatus.Pass, "table"),
+            TestResult("ephemeral_true", TestStatus.Fail, None),
+            TestResult("ephemeral_false", TestStatus.Fail, None),
+            TestResult("ephemeral_unset", TestStatus.Fail, None),
+            # CrateDB adjustment: Use `TestStatus.Pass` instead of `TestStatus.Fail`.
+            TestResult("unset_true", TestStatus.Pass, "table"),
+            TestResult("unset_false", TestStatus.Fail, None),
+            TestResult("unset_unset", TestStatus.Fail, None),
+        }
+        self.run_and_assert(project, expected_results)
 
 
 class TestStoreTestFailuresAsProjectLevelOff(StoreTestFailuresAsProjectLevelOff, PostgresMixin):
-    pass
+    def test_tests_run_successfully_and_are_stored_as_expected(self, project):
+        expected_results = {
+            TestResult("results_view", TestStatus.Fail, "view"),
+            # CrateDB adjustment: Use `TestStatus.Pass` instead of `TestStatus.Fail`.
+            TestResult("results_table", TestStatus.Pass, "table"),
+            TestResult("results_ephemeral", TestStatus.Fail, None),
+            TestResult("results_unset", TestStatus.Fail, None),
+        }
+        self.run_and_assert(project, expected_results)
 
 
 class TestStoreTestFailuresAsProjectLevelView(StoreTestFailuresAsProjectLevelView, PostgresMixin):
@@ -39,7 +67,19 @@ class TestStoreTestFailuresAsProjectLevelEphemeral(
 
 
 class TestStoreTestFailuresAsGeneric(StoreTestFailuresAsGeneric, PostgresMixin):
-    pass
+    def test_tests_run_successfully_and_are_stored_as_expected(self, project):
+        expected_results = {
+            # `store_failures` unset, `store_failures_as = "view"`
+            TestResult("not_null_chipmunks_name", TestStatus.Pass, "view"),
+            # CrateDB adjustment: Use `TestStatus.Pass` instead of `TestStatus.Fail`.
+            # `store_failures = False`, `store_failures_as = "table"`
+            TestResult(
+                "accepted_values_chipmunks_name__alvin__simon__theodore", TestStatus.Pass, "table"
+            ),
+            # `store_failures = True`, `store_failures_as = "view"`
+            TestResult("not_null_chipmunks_shirt", TestStatus.Fail, "view"),
+        }
+        self.run_and_assert(project, expected_results)
 
 
 class TestStoreTestFailuresAsExceptions(StoreTestFailuresAsExceptions, PostgresMixin):
